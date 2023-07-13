a78cd1af3dcfac13026f9cbba850e1bdb4f56261
jenil savani <74345702+jenilsavani9@users.noreply.github.com>
2023-07-13 15:15:20 +0530

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jenil Savani
         </a>
       </header>
     </div>
